2c65df55b8a1b17496c58bde6270c72f5d6a4074
KJastrz1 <147301145+KJastrz1@users.noreply.github.com>
2023-12-06 18:24:41 +0100

    Update Program.cs

--- P05Shop.API/Program.cs
+++ P05Shop.API/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddSwaggerGen();
 
 //Microsoft.EntityFrameworkCore.SqlServer
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnectionString")));
+    options.UseSqlServer(Environment.GetEnvironmentVariable("DBConnectionString")));
 // builder.Configuration.GetConnectionString("DBConnectionString"))  Environment.GetEnvironmentVariable("DBConnectionString"))                                         
 
 builder.Services.AddScoped<IMovieService, P05Shop.API.Services.MovieService.MovieService>();
