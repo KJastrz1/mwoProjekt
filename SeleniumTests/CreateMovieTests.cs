11097f49a3ab8379402744c428443a0bd025b107
Krzysztof Jastrzębski <krzysztof@jastrzebski>
2023-12-07 14:32:27 +0100

    3

--- SeleniumTests/CreateMovieTests.cs
+++ SeleniumTests/CreateMovieTests.cs
@@ -1,64 +1,62 @@
-﻿// using NUnit.Framework;
-// using OpenQA.Selenium;
-// using OpenQA.Selenium.Chrome;
-// using OpenQA.Selenium.Firefox;
-// using OpenQA.Selenium.Support.UI;
-// using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 
-// namespace CreateMovieTests
-// {
-//     [TestFixture]
-//     public class CreateMovieTests
-//     {
-//         private IWebDriver _driver;
-//         private string _appBaseUrl;
+namespace CreateMovieTests
+{
+    [TestFixture]
+    public class CreateMovieTests
+    {
+        private IWebDriver _driver;
+        private string _appBaseUrl;
 
-//         [SetUp]
-//         public void Setup()
-//         {
-//             string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
-//             FirefoxOptions opts = new FirefoxOptions();
-//             opts.AddArgument("--headless");
-//             _driver = new FirefoxDriver(path + @"/Driver", opts);
+        [SetUp]
+        public void Setup()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless"); 
 
-//             _appBaseUrl = "http://localhost:7255/";
-//             _driver.Navigate().GoToUrl(_appBaseUrl + "Movies/Create");
-//         }
+            _driver = new ChromeDriver(options);
+            _appBaseUrl = "http://localhost:7255/";
+            _driver.Navigate().GoToUrl(_appBaseUrl + "Movies/Create");
+        }
 
-//         [Test]
-//         public void CreateMovie_CorrectForm_Success()
-//         {
-//             _driver.FindElement(By.Id("Title")).SendKeys("Test Movie");
-//             _driver.FindElement(By.Id("Director")).SendKeys("Test Director");
-//             _driver.FindElement(By.CssSelector("input[type='submit']")).Click();
+        [Test]
+        public void CreateMovie_CorrectForm_Success()
+        {     
+            _driver.FindElement(By.Id("Title")).SendKeys("Test Movie");
+            _driver.FindElement(By.Id("Director")).SendKeys("Test Director");
+            _driver.FindElement(By.CssSelector("input[type='submit']")).Click();
 
+     
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => driver.Url.Contains("Movies"));
 
-//             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-//             wait.Until(driver => driver.Url.Contains("Movies"));
+           
+            Assert.AreEqual(_appBaseUrl + "Movies", _driver.Url);
+        }
 
+        [Test]
+        public void CreateMovie_Cancel()
+        {
+            _driver.FindElement(By.Id("Title")).SendKeys("Test Movie");
+            _driver.FindElement(By.Id("Director")).SendKeys("Test Director");
 
-//             Assert.AreEqual(_appBaseUrl + "Movies", _driver.Url);
-//         }
+          
+            _driver.FindElement(By.PartialLinkText("Back to List")).Click();
 
-//         [Test]
-//         public void CreateMovie_Cancel()
-//         {
-//             _driver.FindElement(By.Id("Title")).SendKeys("Test Movie");
-//             _driver.FindElement(By.Id("Director")).SendKeys("Test Director");
-
-
-//             _driver.FindElement(By.PartialLinkText("Back to List")).Click();
-
-//             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-//             wait.Until(driver => driver.Url.Contains("Movies"));
-//             Assert.AreEqual(_appBaseUrl + "Movies", _driver.Url);
-//         }
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => driver.Url.Contains("Movies"));
+            Assert.AreEqual(_appBaseUrl + "Movies", _driver.Url);
+        }
 
 
-//         [TearDown]
-//         public void Teardown()
-//         {
-//             _driver.Close();
-//         }
-//     }
-// }
+        [TearDown]
+        public void Teardown()
+        { 
+            _driver.Quit();
+        }
+    }
+}
--- SeleniumTests/MoviesListTests.cs
+++ SeleniumTests/MoviesListTests.cs
@@ -1,90 +1,90 @@
-using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
-using System;
+// using NUnit.Framework;
+// using OpenQA.Selenium;
+// using OpenQA.Selenium.Chrome;
+// using OpenQA.Selenium.Firefox;
+// using OpenQA.Selenium.Support.UI;
+// using System;
 
-namespace MoviesListTests
-{
-    [TestFixture]
-    public class MoviesListTests
-    {
-        private IWebDriver _driver;
-        private string _appBaseUrl;
+// namespace MoviesListTests
+// {
+//     [TestFixture]
+//     public class MoviesListTests
+//     {
+//         private IWebDriver _driver;
+//         private string _appBaseUrl;
 
-        [SetUp]
-        public void Setup()
-        {
-            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;       
-            FirefoxOptions opts = new FirefoxOptions();
-            opts.AddArgument("--headless");
-            _driver = new FirefoxDriver(path + @"/Driver", opts);
-
-
-            _appBaseUrl = "http://localhost:7255/";
-            _driver.Navigate().GoToUrl(_appBaseUrl + "Movies/");
-        }
-
-        [Test]
-        public void MoviesPageTitleIsCorrect()
-        {
-            Assert.AreEqual("Movie", _driver.Title);
-        }
-
-        [Test]
-        public void MoviesTableHasCorrectHeaders()
-        {
-            var tableHeaders = _driver.FindElements(By.TagName("th"));
-            Assert.AreEqual(2, tableHeaders.Count);
-        }
-
-        [Test]
-        public void LinkToCreateMovieWorks()
-        {
-
-            var createNewLink = _driver.FindElement(By.LinkText("Create New"));
-            Assert.IsNotNull(createNewLink);
-        }
-
-        [Test]
-        public void LinkToEditMovieWorks()
-        {
-
-            var firstEditLink = _driver.FindElement(By.CssSelector("table tbody tr:first-child td:nth-child(3) a:first-child"));
-            firstEditLink.Click();
-            Assert.AreEqual(_appBaseUrl + "Movies/Edit/1", _driver.Url);
-        }
+//         [SetUp]
+//         public void Setup()
+//         {
+//             string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;       
+//             FirefoxOptions opts = new FirefoxOptions();
+//             opts.AddArgument("--headless");
+//             _driver = new FirefoxDriver(path + @"/Driver", opts);
 
 
-        [Test]
-        public void LinkToMovieDetailsWorks()
-        {
-            _driver.Navigate().GoToUrl(_appBaseUrl + "Movies/");
-            var firstDetailsLink = _driver.FindElement(By.CssSelector("table tbody tr:first-child td:nth-child(3) a:nth-child(2)"));
-            firstDetailsLink.Click();
-            Assert.AreEqual(_appBaseUrl + "Movies/Details/1", _driver.Url);
-        }
+//             _appBaseUrl = "http://localhost:7255/";
+//             _driver.Navigate().GoToUrl(_appBaseUrl + "Movies/");
+//         }
 
-        [Test]
-        public void LinkToDeleteMovieWorks()
-        {
-            _driver.Navigate().GoToUrl(_appBaseUrl + "Movies/");
-            var firstDeleteLink = _driver.FindElement(By.CssSelector("table tbody tr:first-child td:nth-child(3) a:nth-child(3)"));
-            firstDeleteLink.Click();
-            Assert.AreEqual(_appBaseUrl + "Movies/Delete/1", _driver.Url);
-        }
+//         [Test]
+//         public void MoviesPageTitleIsCorrect()
+//         {
+//             Assert.AreEqual("Movie", _driver.Title);
+//         }
+
+//         [Test]
+//         public void MoviesTableHasCorrectHeaders()
+//         {
+//             var tableHeaders = _driver.FindElements(By.TagName("th"));
+//             Assert.AreEqual(2, tableHeaders.Count);
+//         }
+
+//         [Test]
+//         public void LinkToCreateMovieWorks()
+//         {
+
+//             var createNewLink = _driver.FindElement(By.LinkText("Create New"));
+//             Assert.IsNotNull(createNewLink);
+//         }
+
+//         [Test]
+//         public void LinkToEditMovieWorks()
+//         {
+
+//             var firstEditLink = _driver.FindElement(By.CssSelector("table tbody tr:first-child td:nth-child(3) a:first-child"));
+//             firstEditLink.Click();
+//             Assert.AreEqual(_appBaseUrl + "Movies/Edit/1", _driver.Url);
+//         }
 
 
+//         [Test]
+//         public void LinkToMovieDetailsWorks()
+//         {
+//             _driver.Navigate().GoToUrl(_appBaseUrl + "Movies/");
+//             var firstDetailsLink = _driver.FindElement(By.CssSelector("table tbody tr:first-child td:nth-child(3) a:nth-child(2)"));
+//             firstDetailsLink.Click();
+//             Assert.AreEqual(_appBaseUrl + "Movies/Details/1", _driver.Url);
+//         }
+
+//         [Test]
+//         public void LinkToDeleteMovieWorks()
+//         {
+//             _driver.Navigate().GoToUrl(_appBaseUrl + "Movies/");
+//             var firstDeleteLink = _driver.FindElement(By.CssSelector("table tbody tr:first-child td:nth-child(3) a:nth-child(3)"));
+//             firstDeleteLink.Click();
+//             Assert.AreEqual(_appBaseUrl + "Movies/Delete/1", _driver.Url);
+//         }
 
 
 
 
-        [TearDown]
-        public void Teardown()
-        {
 
-            _driver.Close();
-        }
-    }
-}
+
+//         [TearDown]
+//         public void Teardown()
+//         {
+
+//             _driver.Close();
+//         }
+//     }
+// }
