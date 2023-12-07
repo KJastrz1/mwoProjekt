1040eefbb7a66cf9a7957a0c1a04c30a5a6fd38c
Krzysztof Jastrzębski <krzysztof@jastrzebski>
2023-12-07 15:16:40 +0100

    3

--- SeleniumTests/CreateMovieTests.cs
+++ SeleniumTests/CreateMovieTests.cs
@@ -38,19 +38,19 @@ namespace CreateMovieTests
             Assert.AreEqual(_appBaseUrl + "Movies", _driver.Url);
         }
 
-        // [Test]
-        // public void CreateMovie_Cancel()
-        // {
-        //     _driver.FindElement(By.Id("Title")).SendKeys("Test Movie");
-        //     _driver.FindElement(By.Id("Director")).SendKeys("Test Director");
+        [Test]
+        public void CreateMovie_Cancel()
+        {
+            _driver.FindElement(By.Id("Title")).SendKeys("Test Movie");
+            _driver.FindElement(By.Id("Director")).SendKeys("Test Director");
 
           
-        //     _driver.FindElement(By.PartialLinkText("Back to List")).Click();
+            _driver.FindElement(By.PartialLinkText("Back to List")).Click();
 
-        //     var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-        //     wait.Until(driver => driver.Url.Contains("Movies"));
-        //     Assert.AreEqual(_appBaseUrl + "Movies", _driver.Url);
-        // }
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => driver.Url.Contains("Movies"));
+            Assert.AreEqual(_appBaseUrl + "Movies", _driver.Url);
+        }
 
 
         [TearDown]
