c41e4ab44e518807d2dfb01437c45ee5b6ce315b
Krzysztof Jastrzębski <krzysztof@jastrzebski>
2023-12-07 16:03:58 +0100

    3

--- SeleniumTests/CreateMovieTests.cs
+++ SeleniumTests/CreateMovieTests.cs
@@ -1,67 +1,67 @@
-﻿using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using System;
+﻿// using NUnit.Framework;
+// using OpenQA.Selenium;
+// using OpenQA.Selenium.Chrome;
+// using OpenQA.Selenium.Support.UI;
+// using System;
 
-namespace CreateMovieTests
-{
-    [TestFixture]
-    public class CreateMovieTests
-    {
-        private IWebDriver _driver;
-        private string _appBaseUrl;
+// namespace CreateMovieTests
+// {
+//     [TestFixture]
+//     public class CreateMovieTests
+//     {
+//         private IWebDriver _driver;
+//         private string _appBaseUrl;
 
-        [SetUp]
-        public void Setup()
-        {
-            ChromeOptions options = new ChromeOptions();
-            options.AddArgument("--headless");
-            options.AddArgument("--disable-gpu");  // Add this line for headless mode
+//         [SetUp]
+//         public void Setup()
+//         {
+//             ChromeOptions options = new ChromeOptions();
+//             options.AddArgument("--headless");
+//             options.AddArgument("--disable-gpu");  // Add this line for headless mode
 
-            var service = ChromeDriverService.CreateDefaultService();
-            service.Port = 4445;  // Set the port to a fixed value
+//             var service = ChromeDriverService.CreateDefaultService();
+//             service.Port = 4445;  // Set the port to a fixed value
 
-            _driver = new ChromeDriver(service, options);
+//             _driver = new ChromeDriver(service, options);
 
-            _appBaseUrl = "http://localhost:7255/";
-            _driver.Navigate().GoToUrl(_appBaseUrl + "Movies/Create");
-        }
+//             _appBaseUrl = "http://localhost:7255/";
+//             _driver.Navigate().GoToUrl(_appBaseUrl + "Movies/Create");
+//         }
 
-        [Test]
-        public void CreateMovie_CorrectForm_Success()
-        {
-            _driver.FindElement(By.Id("Title")).SendKeys("Test Movie");
-            _driver.FindElement(By.Id("Director")).SendKeys("Test Director");
-            _driver.FindElement(By.CssSelector("input[type='submit']")).Click();
-
-
-            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-            wait.Until(driver => driver.Url.Contains("Movies"));
+//         [Test]
+//         public void CreateMovie_CorrectForm_Success()
+//         {
+//             _driver.FindElement(By.Id("Title")).SendKeys("Test Movie");
+//             _driver.FindElement(By.Id("Director")).SendKeys("Test Director");
+//             _driver.FindElement(By.CssSelector("input[type='submit']")).Click();
 
 
-            Assert.AreEqual(_appBaseUrl + "Movies", _driver.Url);
-        }
-
-        [Test]
-        public void CreateMovie_Cancel()
-        {
-            _driver.FindElement(By.Id("Title")).SendKeys("Test Movie");
-            _driver.FindElement(By.Id("Director")).SendKeys("Test Director");
+//             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+//             wait.Until(driver => driver.Url.Contains("Movies"));
 
 
-            _driver.FindElement(By.PartialLinkText("Back to List")).Click();
+//             Assert.AreEqual(_appBaseUrl + "Movies", _driver.Url);
+//         }
 
-            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-            wait.Until(driver => driver.Url.Contains("Movies"));
-            Assert.AreEqual(_appBaseUrl + "Movies", _driver.Url);
-        }
+//         [Test]
+//         public void CreateMovie_Cancel()
+//         {
+//             _driver.FindElement(By.Id("Title")).SendKeys("Test Movie");
+//             _driver.FindElement(By.Id("Director")).SendKeys("Test Director");
 
 
-        [TearDown]
-        public void Teardown()
-        {
-            _driver.Quit();
-        }
-    }
-}
+//             _driver.FindElement(By.PartialLinkText("Back to List")).Click();
+
+//             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+//             wait.Until(driver => driver.Url.Contains("Movies"));
+//             Assert.AreEqual(_appBaseUrl + "Movies", _driver.Url);
+//         }
+
+
+//         [TearDown]
+//         public void Teardown()
+//         {
+//             _driver.Quit();
+//         }
+//     }
+// }
